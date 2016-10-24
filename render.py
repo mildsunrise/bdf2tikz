27254c0cf38960cbc27ca37e78c873383cd68aeb
Alba Mendez <me@alba.sh>
2016-10-24 21:08:11 +0200

    Remove obsoleted task

--- render.py
+++ render.py
@@ -87,7 +87,6 @@ def calculate_optimal_anchor_to_line(bounds, vertical, line):
 
   p1 = (line.p1.x, line.p1.y)
   p2 = (line.p2.x, line.p2.y)
-  # FIXME: prefer centered anchors
   anchors = TEXT_ANCHORS.keys()
   anchors.sort(key=lambda anchor: abs(TEXT_ANCHORS[anchor][0]) + abs(TEXT_ANCHORS[anchor][1]))
   anchors.sort(key=lambda anchor: distance_to_segment(p1, p2, calculate_anchor_point(bounds, vertical, anchor)))
