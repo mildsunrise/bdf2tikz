5c906fd3ead344e00213d7dd8fd96a20468dc1fa
Alba Mendez <me@alba.sh>
2016-10-30 11:31:52 +0100

    Make main executable, and also usable as module

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+#!/usr/bin/env python
+
 import sys
 from parser import parse_bdf
 import render, parser
 import pprint
 
-rs = parse_bdf(open(sys.argv[1],"rb").read())
-
-options = {
+default_options = {
   "scale": 1/42.,
   "port_name_t_snap": 8, "port_name_n_snap": 12, "port_name_n_distance": 4,
   "anchor_ports": True, "anchor_labels": True,
@@ -15,30 +15,38 @@ options = {
 
   "offset": (0,0), "extra_args": [],
 }
-lines = []
-output = ""
-complementary_output = ""
 
-for thing in rs:
-  if isinstance(thing, parser.Pin):
-    output += render.render_tikz_comment("Pin (%s) named %s" % (thing.typeText.text, thing.name.text), options)
-    output += render.render_pin(lines, thing, options) + "\n"
-  elif isinstance(thing, parser.Symbol):
-    output += render.render_tikz_comment("Symbol (%s) named %s" % (thing.typeText.text, thing.name.text), options)
-    output += render.render_symbol(lines, thing, options) + "\n"
-  elif isinstance(thing, parser.Text):
-    output += render.render_text(thing, options) + "\n"
-  elif isinstance(thing, parser.Junction):
-    complementary_output += render.render_junction(thing, options)
-  elif isinstance(thing, parser.Connector):
-    tmp = render.render_connector(lines, thing, options)
-    if tmp: complementary_output += tmp
-  else:
-    print "WARNING: couldn't process object of type %s in schematic" % (type(thing),)
+def render_bdf(rs, options):
+	rs = parse_bdf(rs)
+	lines = []
+	output = ""
+	complementary_output = ""
 
-output += render.render_all_lines(lines, options)
-#for line in lines:
-#  output += render.render_tikz_statement([], "%s -- %s" % (render.render_tikz_point(line[0], options), render.render_tikz_point(line[1], options)), options)
-output += complementary_output
+	for thing in rs:
+		if isinstance(thing, parser.Pin):
+		  output += render.render_tikz_comment("Pin (%s) named %s" % (thing.typeText.text, thing.name.text), options)
+		  output += render.render_pin(lines, thing, options) + "\n"
+		elif isinstance(thing, parser.Symbol):
+		  output += render.render_tikz_comment("Symbol (%s) named %s" % (thing.typeText.text, thing.name.text), options)
+		  output += render.render_symbol(lines, thing, options) + "\n"
+		elif isinstance(thing, parser.Text):
+		  output += render.render_text(thing, options) + "\n"
+		elif isinstance(thing, parser.Junction):
+		  complementary_output += render.render_junction(thing, options)
+		elif isinstance(thing, parser.Connector):
+		  tmp = render.render_connector(lines, thing, options)
+		  if tmp: complementary_output += tmp
+		else:
+		  print "WARNING: couldn't process object of type %s in schematic" % (type(thing),)
 
-open(sys.argv[2], "w").write(output)
+	output += render.render_all_lines(lines, options)
+	output += complementary_output
+
+	return output
+
+
+
+if __name__ == "__main__":
+  rs = open(sys.argv[1],"rb").read()
+  output = render_bdf(rs, default_options)
+  open(sys.argv[2], "w").write(output)
